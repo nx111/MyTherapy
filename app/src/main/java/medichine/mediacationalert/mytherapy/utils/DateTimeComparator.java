e4882843bb9a88f1f1a4e9e9867eaf3279448dfb
nx111 <gd.zhangdz@gmail.com>
2026-05-11 08:50:55 +0800

    Fix reminder date sorting format

--- app/src/main/java/medichine/mediacationalert/mytherapy/utils/DateTimeComparator.java
+++ app/src/main/java/medichine/mediacationalert/mytherapy/utils/DateTimeComparator.java
@@ -4,13 +4,14 @@ import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Comparator;
+import java.util.Locale;
 
-public class DateTimeComparator implements Comparator {
-    DateFormat f = new SimpleDateFormat("dd/mm/yyyy hh:mm");
+public class DateTimeComparator implements Comparator<DateTimeSorter> {
+    DateFormat f = new SimpleDateFormat("d/M/yyyy H:mm", Locale.US);
 
-    public int compare(Object a, Object b) {
-        String o1 = ((DateTimeSorter)a).getDateTime();
-        String o2 = ((DateTimeSorter)b).getDateTime();
+    public int compare(DateTimeSorter a, DateTimeSorter b) {
+        String o1 = a.getDateTime();
+        String o2 = b.getDateTime();
 
         try {
             return f.parse(o1).compareTo(f.parse(o2));
@@ -19,4 +20,3 @@ public class DateTimeComparator implements Comparator {
         }
     }
 }
-
